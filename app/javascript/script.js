39093d4b1c505f2931ffce4993ed26d18b415a1d
Albano <albigoal@outlook.com>
2024-09-04 17:31:24 +0200

    canvas mouse down begin path

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,3 +1,12 @@
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 let drawing = false;
+//ctx.beginPath();
+//ctx.moveTo(50, 50);
+//ctx.lineTo(200, 200);
+//ctx.stroke();
+//test draw
+
+canvas.addEventListener('mousedown', function(e) {
+            drawing = true;ctx.beginPath();ctx.moveTo(e.offsetX, e.offsetY);
+});

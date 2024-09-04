b7c383daf294ed6ee7b565cfb088b496340c299d
Albano <albigoal@outlook.com>
2024-09-04 12:51:46 +0200

    seq no

--- src/model.py
+++ src/model.py
@@ -1,1 +1,25 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Input
 
+model = Sequential()
+print("Sequential model imported successfully")
+
+#geht nicht
+def create_model():
+
+    model = Sequential([
+        Input(shape=(28, 28, 1)),
+        Conv2D(32, (3, 3), activation='relu'),
+        MaxPooling2D((2, 2)),
+        Conv2D(64, (3, 3), activation='relu'),
+        MaxPooling2D((2, 2)),
+        Flatten(),
+        Dense(128, activation='relu'),
+        Dense(10, activation='softmax')
+    ])
+
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    return model
---
+++ test_seq.py
@@ -0,0 +1,4 @@
+
+from tensorflow.keras.models import Sequential
+model = Sequential()
+print("Sequential Model wurde erfolgreich erstellt!")
